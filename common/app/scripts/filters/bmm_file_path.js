ca974dcb32e9375b0129bb5e249a8c652d33a296
steffan <steffanhalv@gmail.com>
2014-06-19 20:50:59 +0200

    Added note for where this filter is used

--- common/app/scripts/filters/bmm_file_path.js
+++ common/app/scripts/filters/bmm_file_path.js
@@ -4,6 +4,8 @@ angular.module('bmmLibApp')
   .filter('bmmFilePath', ['bmmApi', function (bmmApi) {
     return function (path) {
 
+      //Used to secure file before playing track in admin -> track controller
+
       if (path.substring(0,4)!=='http') {
         path = bmmApi.getserverUrli()+'file/protected/'+path;
       }
