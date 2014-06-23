39e7bc36a310cc52c82a62ae1888bfb3b431930d
steffan <steffanhalv@gmail.com>
2014-06-23 20:50:17 +0200

    Removed file securing from this filter. File securing should now be applied in controller scope.

--- common/app/scripts/filters/bmm_file_path.js
+++ common/app/scripts/filters/bmm_file_path.js
@@ -10,7 +10,7 @@ angular.module('bmmLibApp')
         path = bmmApi.getserverUrli()+'file/protected/'+path;
       }
 
-      path = bmmApi.secureFile(path);
+      path = path;
 
       return path;
     };
