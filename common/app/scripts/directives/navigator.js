5b0cc603754ba735c878349e49adf2fd4556ce73
steffan <steffanhalv@gmail.com>
2014-08-19 15:19:44 +0200

    Fixed navigator so it works with push messages

--- common/app/scripts/directives/navigator.js
+++ common/app/scripts/directives/navigator.js
@@ -1,50 +1,61 @@
 'use strict';
 
 angular.module('bmmLibApp')
-  .directive('navigator', [function () {
+  .directive('navigator', ['$timeout', function ($timeout) {
     return {
       link: function postLink(scope, element) {
+
+        /**
+         * This fixes some styling issues on resize caused by the changing topbar
+         * @todo - Experiment with CSS and try to leave this out, unwanted code..
+         */
 
         scope.absolute = false;
         scope.fixed = false;
         scope.fixedSmall = false;
         scope.visible = false;
 
-        //Fix navigation on scroll
-        scope.fixed = false;
-
-        $(window).scroll(function() {
-          scope.$apply(function() {
-            fixNav();
-          });
-        });
-
         var fixNav = function() {
 
-          if ($('[navigator]').height()<($('[ng-view]').height()+$('header').height()+$('.push-messages').height())) {
-            if ($(document).scrollTop()>=($('header').height()+$('#sw-topbar').height()+$('.push-messages').height())) {
-              scope.fixedSmall = true;
-            } else {
-              scope.fixedSmall = false;
-            }
-            if ($(document).scrollTop()>=$('#sw-topbar').height()) {
-              scope.absolute = false;
+          scope.$apply(function() {
+            if ($('[navigator]').height()<(
+                  $('[ng-view]').height()+
+                  $('header').height()+
+                  $('.push-messages').height()
+                )) {
+              if ($(document).scrollTop()>=(
+                    $('header').height()+
+                    $('#sw-topbar').height()+
+                    $('.push-messages').height()
+                )) {
+                scope.fixedSmall = true;
+              } else {
+                scope.fixedSmall = false;
+              }
+              if ($(document).scrollTop()>=$('#sw-topbar').height()) {
+                scope.absolute = false;
+              } else {
+                scope.absolute = true;
+              }
+              if ($(document).scrollTop()>=(
+                    $('header').height()+
+                    $('.push-messages').height()
+                  )) {
+                scope.fixed = true;
+              } else {
+                scope.fixed = false;
+              }
             } else {
               scope.absolute = true;
-            }
-            if ($(document).scrollTop()>=($('header').height()+$('.push-messages').height())) {
-              scope.fixed = true;
-            } else {
               scope.fixed = false;
+              scope.fixedSmall = false;
             }
-          } else {
-            scope.absolute = true;
-            scope.fixed = false;
-            scope.fixedSmall = false;
-          }
+          });
 
         };
-        fixNav();
+
+        $timeout(fixNav);
+        $(window).on('scroll', fixNav);
 
       }
     };
