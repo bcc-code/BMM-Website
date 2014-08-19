12b372aecd2bf1db09e76cfa102da1169ee4908c
steffan <steffanhalv@gmail.com>
2014-08-19 15:17:29 +0200

    Optimized click elsewhere

--- common/app/scripts/directives/click_elsewhere.js
+++ common/app/scripts/directives/click_elsewhere.js
@@ -5,13 +5,28 @@ angular.module('bmmLibApp')
     return {
       link: function postLink(scope, element, attr) {
 
+        /**
+         * Use like:
+         * <div click-elsewhere="yourFunction();"></div>
+         * With optional attribute:(Explained in code)
+         * <div click-elsewhere="yourFunction();" always></div>
+         */
+
         var elsewhere = true;
 
-        element.on('click', function(e) {
-          elsewhere = false
-          $document.off('click', clickElsewhere);
-          $document.on('click', clickElsewhere);
-        });
+        if (typeof attr.always==='undefined'||!attr.always) {
+          //clickElsewhere listener is created on click and killed on clickElsewhere
+          //Using alot of directives with this option doesnt affect the performance
+          element.on('click', function(e) {
+            elsewhere = false
+            $document.off('click', clickElsewhere);
+            $document.on('click', clickElsewhere);
+          });
+        } else {
+          //clickElsewhereAlways listener will be created instantly and never killed
+          //Warning! Using alot of directives with this option makes bad performance
+          $document.on('click', clickElsewhereAlways);
+        }
 
         var clickElsewhere = function() {
           if (elsewhere) {
@@ -19,6 +34,10 @@ angular.module('bmmLibApp')
             $document.off('click', clickElsewhere);
           }
           elsewhere = true;
+        };
+
+        var clickElsewhereAlways = function() {
+          scope.$apply(attr.clickElsewhere);
         };
 
       }
