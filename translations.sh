dac9beffed12acaec9b9ed366489faafd4ad9084
Felix Schoeler <felix@hectobyte.net>
2017-06-28 15:12:01 +0200

    Updated script to copy new notifications translation file.

--- translations.sh
+++ translations.sh
@@ -9,8 +9,12 @@ do
     [ -f "$language/admin_en.json" ] && mv $language/admin_en.json ../admin/app/translations/$language.json
     [ -f "$language/bible_en.json" ] && mv $language/bible_en.json ../admin/app/translations/bible/$language.json
     [ -f "$language/BMM-Website.json" ] && mv $language/BMM-Website.json ../client/app/translations/$language.json
+    [ -f "$language/main.json" ] && mkdir -p ../translations_external/App/$language; mv $language/main.json $_
+    [ -f "$language/notifications.json" ] && mkdir -p ../translations_external/Api/notifications/$language; mv $language/notifications.json $_
 done
 
 cd ..
 
 php translations.php
+
+rm -r translations
