59dde1e45ef814e1ff66f0d570bcc4e968977889
Simon Costea <simon.costea@bcc.no>
2018-12-18 18:10:17 +0200

    Implement support for batchPeriod

--- admin/app/scripts/controllers/notifications.js
+++ admin/app/scripts/controllers/notifications.js
@@ -9,8 +9,9 @@ angular.module('bmmApp')
   ) {
   $scope.init = _init;
   $scope.anyNotification = false;
+  $scope.graceTime = 0;
+  $scope.batchPeriod = 0;
 
-  var graceTime = _init.config.graceTimeNotificationJob;
   var timeBetweenNotificationJobs = 5;
   var currentDatetime = new Date();
 
@@ -18,14 +19,16 @@ angular.module('bmmApp')
     $scope.loading = false;
   }
 
-  function checkNotificationBatchAlreadyExists(notifications, batchDate) {
+  function checkNotificationBatchAlreadyExists(notifications, scheduledTime) {
     var batchIndex = -1;
+
     notifications.forEach(function(notification, index) {
-      if (notification.scheduledTime == batchDate) {
+      if (Math.abs(scheduledTime - notification.scheduledTime) / 60000 <= $scope.batchPeriod) {
         batchIndex = index;
         return;
       }
     });
+
     return batchIndex;
   }
 
@@ -37,37 +40,36 @@ angular.module('bmmApp')
       $scope.podcasts = podcasts;
 
         podcasts.forEach(function(podcast) {
-          _api.nextTracksToBePublished(podcast.id).then(function(tracks) { // Will get just unnotified tracks
+          _api.nextTracksToBePublished(podcast.id).then(function(viewModel) { // Will get just unnotified tracks
             podcast.notifications = [];
+            $scope.graceTime = viewModel.grace_time_notification_job;
+            $scope.batchPeriod = viewModel.notification_batch_period;
 
-            tracks.forEach(function(track) {
+            viewModel.tracks.forEach(function(track) {
               $scope.anyNotification = true;
 
               var graceTimeWasUsed;
               var lastModifiedAt = new Date(track._meta.modified_at > track.published_at ? track._meta.modified_at : track.published_at);
               var scheduledTime = lastModifiedAt.getTime();
 
-              if (Math.round((((new Date(track.published_at) - new Date(track._meta.modified_at)) % 86400000) % 3600000) / 60000) > graceTime) {
+              if (Math.round((((new Date(track.published_at) - new Date(track._meta.modified_at)) % 86400000) % 3600000) / 60000) > $scope.graceTime) {
                   // The track was published with a DateTime in the future
                   graceTimeWasUsed = false;
               } else {
-                  // The track was published or last modified in the near future (less than the graceTime) or in the past
-                  scheduledTime += graceTime * 60000;
+                  // The track was published or last modified in the near future (less than the $scope.graceTime) or in the past
+                  scheduledTime += $scope.graceTime * 60000;
                   graceTimeWasUsed = true;
               }
 
-              var minutesTilNextNotificationJob = timeBetweenNotificationJobs - (new Date(scheduledTime).getMinutes() % timeBetweenNotificationJobs);
-              var secondsToRemoveToBeAbleToHaveSameBatchDates = new Date(scheduledTime).getSeconds();
-              var batchDate = scheduledTime + (minutesTilNextNotificationJob * 60000) - (secondsToRemoveToBeAbleToHaveSameBatchDates * 1000);
+              // Check if we should batch the notifications together
+              var indexOfNotificationBatch = checkNotificationBatchAlreadyExists(podcast.notifications, scheduledTime);
 
-              // Check if there is already a notification batch sending on batchDate
-              var indexOfNotificationBatch = checkNotificationBatchAlreadyExists(podcast.notifications, batchDate);
               if (indexOfNotificationBatch != -1) {
                 podcast.notifications[indexOfNotificationBatch].tracks.push(track);
-                podcast.notifications[indexOfNotificationBatch].scheduledTime = batchDate;
                 podcast.notifications[indexOfNotificationBatch].graceTimeWasUsed = graceTimeWasUsed;
+                podcast.notifications[indexOfNotificationBatch].scheduledTime = Math.max(scheduledTime, podcast.notifications[indexOfNotificationBatch].scheduledTime);
               } else {
-                podcast.notifications.push({ tracks: [track], scheduledTime: batchDate, graceTimeWasUsed: graceTimeWasUsed });
+                podcast.notifications.push({ tracks: [track], scheduledTime: scheduledTime, graceTimeWasUsed: graceTimeWasUsed });
               }
 
               track.languages = [];
@@ -79,6 +81,10 @@ angular.module('bmmApp')
                   track.languages.push(translation.language);
                 }
               });
+            });
+
+            podcast.notifications.sort(function(a, b){
+              return a.scheduledTime - b.scheduledTime;
             });
 
             if (++podcastsLoaded == podcasts.length) {
