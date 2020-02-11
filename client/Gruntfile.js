73b3f26e82e1e038752b71becc1178c9f97678df
Karsten Küpper <karsten.kuepper@bcc.no>
2020-02-11 20:52:37 +0100

    use http for local development

--- client/Gruntfile.js
+++ client/Gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function (grunt) {
         port: 9001,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
-        protocol: 'https',
+        protocol: 'http',
         livereload: 35729
       },
       livereload: {
