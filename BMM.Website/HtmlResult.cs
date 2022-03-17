44c132022eca56550495e0872b1a7f868381f655
Karsten Küpper <karsten.kuepper@bcc.no>
2022-03-17 15:43:37 -0400

    return html and build BMM.Website

---
+++ BMM.Website/HtmlResult.cs
@@ -0,0 +1,31 @@
+using System.Net.Mime;
+using System.Text;
+
+namespace BMM.Website;
+
+public class HtmlResult : IResult
+{
+    private readonly string _indexHtml;
+    private readonly string _title;
+    private readonly string _description;
+    private const string Placeholder = "<!-- {{MetadataPlaceholder}} -->";
+
+    public HtmlResult(string indexHtml, string title, string description)
+    {
+        _indexHtml = indexHtml;
+        _title = title;
+        _description = description;
+    }
+    
+    public Task ExecuteAsync(HttpContext httpContext)
+    {
+        //ToDo: load actual metadata 
+        var metaTags =
+            $"<meta property=\"og:title\" content=\"{_title}\"><meta property=\"og:description\" content=\"{_description}\">\n";
+        var adjustedHtml = _indexHtml.Replace(Placeholder, metaTags);
+        
+        httpContext.Response.ContentType = MediaTypeNames.Text.Html;
+        httpContext.Response.ContentLength = Encoding.UTF8.GetByteCount(adjustedHtml);
+        return httpContext.Response.WriteAsync(adjustedHtml);
+    }
+}
--- BMM.Website/Program.cs
+++ BMM.Website/Program.cs
@@ -1,8 +1,16 @@
+using BMM.Website;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var indexFile = File.ReadAllText("wwwroot/index.html");
+
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/track/99888", () =>
+{
+    return new HtmlResult(indexFile, "bmm", "Listen to edifying music and messages");
+});
 app.UseStaticFiles();
+app.MapGet("*", () => "catch all");
 
 app.Run();
