cb2d97b9f5f81303a331c090ac37529437d104a6
Karsten Küpper <karsten.kuepper@bcc.no>
2024-02-26 11:58:32 -0500

    try to detect users system and offer a download button

--- client/app/scripts/controllers/download.js
+++ client/app/scripts/controllers/download.js
@@ -15,6 +15,33 @@ angular.module('bmmApp')
     $scope.data = null;
     $scope.load = true;
 
+    $scope.arch = null;
+
+    //ToDo: doesn't work on a lot of devices
+    if (navigator.userAgentData){
+      navigator.userAgentData.getHighEntropyValues(['architecture']).then(function(result){
+        console.log("result", result);
+        $scope.arch = result;
+      });
+    }
+
+    $scope.linkForArchitecture = function(){
+      if ($scope.arch) {
+        if ($scope.arch.platform == "macOS"){
+          if ($scope.arch.architecture == "arm")
+            return $scope.data.mac_arm;
+          return $scope.data.mac_intel;
+        }
+        if ($scope.arch.mobile){
+          if ($scope.arch.platform == "iOS")
+            return $scope.data.ios;
+          return $scope.data.android;
+        }
+      }
+      // Windows seems like a reasonable default value
+      return $scope.data.windows;
+    }
+
     _api.getDownloadLinks().done(function(data){
       $scope.data = data;
       $scope.load = false;
